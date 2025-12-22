e98a4ab28d30202b512853324081a09aac816169
AryaPKusuma <aryakusuma0703@gmail.com>
2025-12-22 16:30:07 +0700

    remove unused  variable in dayOfProgrammer function

--- 18-DayOfProgrammer.js
+++ 18-DayOfProgrammer.js
@@ -1,19 +1,16 @@
 function dayOfProgrammer(year){
-    let leap = false
     let date = `13.09.${year}`
 
     // in year 1918, after 31 january was 14 ferbruary, 
-    // so there was leap or additional day (14 day), if the day of prorammer is 12 september 
+    // so there was leap or additional day (14 day), if day of prorammer is 12 september 
     // that means 12 + 14 is the day of programmer in 1918
     if (year === 1918) return date = '26.09.1918';
 
     if (year < 1918 && year % 4 === 0){
-        leap  =  true
         return date = `12.09.${year}`
     }
 
     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){
-        leap = true
         return date = `12.09.${year}`
     }
 }
